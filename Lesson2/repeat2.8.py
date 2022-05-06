b478a9f6cc9cc15c98b9ecd1622787f955029ea4
IamMushroom <eratoster@gmail.com>
2022-05-06 17:54:42 +0300

    Дописал задачу Иосифа

--- Lesson2/repeat2.8.py
+++ Lesson2/repeat2.8.py
@@ -1,2 +1,9 @@
 n, k = int(input()), int(input())
 live_list = [i + 1 for i in range(n)]
+
+i = 0
+while len(live_list) > 1:
+    pos = (i + k - 1) % len(live_list)
+    del live_list[pos]
+    i = pos
+print(*live_list)
